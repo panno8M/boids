c77a0b121a0f23116d4afaa2698f86b06dc349f4
la .panon. <panno.devel@gmail.com>
2025-09-25 16:56:14 +0900

    update timemeasure

--- nim/gdext-boid/src/timemeasure.nim
+++ nim/gdext-boid/src/timemeasure.nim
@@ -8,21 +8,27 @@ type TimeBuffer*[Steps: static int] = object
   min: float = Inf
   max: float
   iteration: int
+  avg: float
 
 var defaultTimeBuffer* = TimeBuffer[8]()
 
-proc pushavg*[Steps](buffer: var TimeBuffer[Steps]; value: float): string =
+proc `$`*[Steps](buffer: TimeBuffer[Steps]; value: float): string =
+  fmt"[{buffer.iteration.pred}]: {value} (avg: {buffer.avg} min: {buffer.min} max: {buffer.max})"
+
+proc push*[Steps](buffer: var TimeBuffer[Steps]; value: float) =
+  inc buffer.iteration
   buffer.b[buffer.current] = value
   buffer.current = buffer.current.succ mod Steps
   buffer.filled = min(buffer.filled.succ, Steps)
   if buffer.filled == Steps:
     buffer.min = min(buffer.min, value)
   buffer.max = max(buffer.max, value)
-  let avg = sum(buffer.b.toOpenArray(0, buffer.filled.pred)) / buffer.filled
-  result = fmt"[{buffer.iteration}]: {value} (avg: {avg} min: {buffer.min} max: {buffer.max})"
-  inc buffer.iteration
+  buffer.avg = sum(buffer.b.toOpenArray(0, buffer.filled.pred)) / buffer.filled
+
+proc avg*(buffer: var TimeBuffer): float = buffer.avg
 
 template measure*(buffer: TimeBuffer; body): string =
   var start = epochTime()
   body
-  buffer.pushavg(epochTime() - start)
+  buffer.push(epochTime() - start)
+  $buffer
