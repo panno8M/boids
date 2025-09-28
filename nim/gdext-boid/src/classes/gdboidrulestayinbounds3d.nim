31300d424fac221a9b2f78f5d06fe583a5766345
la .panon. <panno.devel@gmail.com>
2025-09-28 11:56:50 +0900

    optimize BoidRuleStayInBounds

--- nim/gdext-boid/src/classes/gdboidrulestayinbounds3d.nim
+++ nim/gdext-boid/src/classes/gdboidrulestayinbounds3d.nim
@@ -4,31 +4,58 @@ import gdext/classes/[gdGridMap]
 import global
 import classes/[gdBoidController3D]
 
+type Cache = object
+  boundsMin: Vector3
+  boundsMax: Vector3
+
 type BoidRuleStayInBounds3D* {.gdsync.} = ptr object of BoidModule3D
   factor* {.gdexport: Appearance.range(0, 1).}: float = 0.2
-  bounds* {.gdexport.}: AABB
-  threshold* {.gdexport.}: float = 1
+  bounds*: AABB
+  threshold*: float = 1
+  cache: Cache
 
-proc axisForce(boundpos, size, threshold, pos: float32): float32 =
-  if unlikely(pos < boundpos + threshold):
-    boundpos + threshold - pos
-  elif unlikely(pos > boundpos + size - threshold):
-    boundpos + size - threshold - pos
+proc createCache(bounds: AABB; threshold: float): Cache =
+  Cache(
+    boundsMin: bounds.position + threshold,
+    boundsMax: bounds.position + bounds.size - threshold,
+  )
+
+gdexport "bounds",
+  proc(self: BoidRuleStayInBounds3D): AABB = self.bounds,
+  proc(self: BoidRuleStayInBounds3D; value: AABB) =
+    self.bounds = value
+    self.cache = createCache(self.bounds, self.threshold)
+
+gdexport "threshold",
+  proc(self: BoidRuleStayInBounds3D): float = self.threshold,
+  proc(self: BoidRuleStayInBounds3D; value: float) =
+    self.threshold = value
+    self.cache = createCache(self.bounds, self.threshold)
+
+method onInit(self: BoidRuleStayInBounds3D) =
+  self.cache = createCache(self.bounds, self.threshold)
+
+proc axisForce(boundsMin, boundsMax, pos: float32): float32 =
+  if unlikely(pos < boundsMin):
+    boundsMin - pos
+  elif unlikely(boundsMax < pos):
+    boundsMax - pos
   else:
     0
 
 proc stayInBounds(self: BoidRuleStayInBounds3D; boid: var Boid) =
   boid.acceleration += vector3(
-    axisForce(self.bounds.position.x, self.bounds.size.x, self.threshold, boid.position.x),
-    axisForce(self.bounds.position.y, self.bounds.size.y, self.threshold, boid.position.y),
-    axisForce(self.bounds.position.z, self.bounds.size.z, self.threshold, boid.position.z),
+    axisForce(self.cache.boundsMin.x, self.cache.boundsMax.x, boid.position.x),
+    axisForce(self.cache.boundsMin.y, self.cache.boundsMax.y, boid.position.y),
+    axisForce(self.cache.boundsMin.z, self.cache.boundsMax.z, boid.position.z),
   ) * self.factor
 
 method update(self: BoidRuleStayInBounds3D; phase: ProcessPhase) {.gdsync.} =
+  if unlikely(self.factor == 0): return
+
   case phase
   of ProcessPhaseAcceleration:
-    if likely(self.factor != 0):
-      for i, boid in self.controller.boids.mpairs:
-        self.stayInBounds(boid)
+    for i, boid in self.controller.boids.mpairs:
+      self.stayInBounds(boid)
   of ProcessPhaseVelocity:
     discard
