3ac37b686e1c926a570c4f16f48094cdd01d1af2
la .panon. <panno.devel@gmail.com>
2025-10-10 15:49:12 +0900

    show info of captured agent on debug status

--- stat.gd
+++ stat.gd
@@ -1,8 +1,12 @@
 extends Control
 
 @export var controller: BoidController3D
+@export var player: Player
 
 func _process(delta: float) -> void:
 	$FPS.text = str(Engine.get_frames_per_second())
 	if controller:
 		$Agents.text = str(controller.get_agent_count())
+	if player:
+		if player.latest_agent:
+			$CurrentAgent/Name.text = player.latest_agent.name
