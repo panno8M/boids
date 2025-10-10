1199fba8cf0bfc159e318c3ce280d52569505e73
la .panon. <panno.devel@gmail.com>
2025-10-10 15:48:31 +0900

    Capture the agent at the center of the camera

--- player.gd
+++ player.gd
@@ -1,10 +1,28 @@
 extends Node3D
+class_name Player
 
 var move_speed := 10.0
 var mouse_sensitivity := 0.2
 var yaw := 0.0
 var pitch := 0.0
 var velocity := Vector3.ZERO
+@export var closest_agent: BoidAgent3D
+@export var latest_agent: BoidAgent3D
+
+func get_closest_agent(camera: Camera3D, distance: float = 1000.0, mask: int = 0xFFFFFFFF) -> BoidAgent3D:
+	var screen_center = get_viewport().size / 2
+
+	var from = camera.project_ray_origin(screen_center)
+	var to = from + camera.project_ray_normal(screen_center) * distance
+
+	var query = PhysicsRayQueryParameters3D.create(from, to)
+	query.collision_mask = mask
+	query.collide_with_areas = true
+
+	var result = get_world_3d().direct_space_state.intersect_ray(query)
+	if result:
+		return result.collider.get_parent() as BoidAgent3D
+	return null
 
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
@@ -40,3 +58,7 @@ func _process(delta):
 		velocity = velocity.limit_length(max(0, velocity.length()-power))
 	if velocity != Vector3.ZERO:
 		translate(velocity * move_speed * delta)
+
+	closest_agent = get_closest_agent($Camera3D)
+	if closest_agent:
+		latest_agent = closest_agent
