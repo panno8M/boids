78d7c01d1ca67890e356ac80befeb874aac1d615
la .panon. <panno.devel@gmail.com>
2025-11-27 21:14:29 +0900

    update vellum

--- book/vellum/vellum.gd
+++ book/vellum/vellum.gd
@@ -2,7 +2,7 @@ extends BookBase
 class_name Vellum
 
 func open_callback() -> void:
-	current_left_page = PageProvider.add_preset_at_once("EmptyPage", "EmptyPage.0")
-	current_right_page = PageProvider.add_preset_at_once("EmptyPage", "EmptyPage.1")
-	swap_left_page = PageProvider.add_preset_at_once("EmptyPage", "EmptyPage.2")
-	swap_right_page = PageProvider.add_preset_at_once("EmptyPage", "EmptyPage.3")
+	current_left_page = PageProvider.add_preset_at_once("TerminalPage", "Vellum")
+	current_right_page = PageProvider.add_preset_at_once("EmptyPage", "EmptyPage.0")
+	swap_left_page = PageProvider.add_preset_at_once("EmptyPage", "EmptyPage.1")
+	swap_right_page = PageProvider.add_preset_at_once("EmptyPage", "EmptyPage.2")
