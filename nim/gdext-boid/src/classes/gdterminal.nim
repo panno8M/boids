d413b95d8ea3e01dbb29e171d0cad4a9f126e049
la .panon. <panno.devel@gmail.com>
2025-11-28 19:20:54 +0900

    update terminal

--- nim/gdext-boid/src/classes/gdterminal.nim
+++ nim/gdext-boid/src/classes/gdterminal.nim
@@ -1,4 +1,4 @@
-import os, osproc, strutils, streams
+import os, osproc, strutils, streams, strformat
 
 import gdext
 import gdext/nameformats
@@ -6,32 +6,64 @@ import gdext/classes/[gdCodeEdit, gdLabel]
 
 import shell
 
+type Caret = object
+  line: int32
+  column: int32
+
+proc getCaret(self: TextEdit): Caret =
+  result.line = self.getCaretLine
+  result.column = self.getCaretColumn
+
+proc setCaret(self: TextEdit; c: Caret) =
+  self.setCaretLine(c.line)
+  self.setCaretColumn(c.column)
+
+proc setCaretWithFill(self: TextEdit; c: Caret) =
+  self.setCaret(c)
+  var res = self.getCaret
+  if res.line < c.line:
+    self.insertTextAtCaret "\n".repeat(c.line - res.line) & " ".repeat(c.column)
+  elif res.column < c.column:
+    self.insertTextAtCaret " ".repeat(c.column - res.column)
+
 type Terminal* {.gdsync.} = ptr object of CodeEdit
   resultBuffer* {.gdexport.}: CodeEdit
   env: ShellEnv
   prevdir: string
   process: Process
+  promptStartAt: Caret
+  caret_prev: Caret
+
+proc prompt(self: Terminal): string = &"""
+
+┬─ {self.env.pwd}
+╰─>$ """
+
+proc insertPrompt(self: Terminal) =
+  self.insertTextAtCaret self.prompt
+  self.promptStartAt = self.getCaret
+  self.scrollVertical = float self.promptStartAt.line
 
 method ready(self: Terminal) {.gdsync.} =
   if self.resultBuffer.isNil: self.resultBuffer = self
-  discard self.connect("text_changed", self.callable"_on_write_buffer_text_changed")
+  discard self.connect("text_changed", self.callable"_on_text_changed")
+  discard self.connect("caret_changed", self.callable"_on_caret_changed")
   self.env.pwd = expandFileName(".")
-  self.text = String(self.env.pwd & " $")
-  self.setCaretColumn(10000)
-  self.setCaretLine(10000)
+  self.insertPrompt
 
 method process(self: Terminal; delta: float64) {.gdsync.} =
   if self.process != nil:
     if self.process.running:
-      self.resultBuffer.text = self.resultBuffer.text + String(self.process.readAvailable)
+      let output = self.process.readAvailable
+      if output.len != 0:
+        self.resultBuffer.insertTextAtCaret output
     else:
       if not self.process.outputStream.atEnd:
-        self.resultBuffer.text = self.resultBuffer.text + String(self.process.outputStream.readAll)
-      self.editable = true
+        let output = self.process.outputStream.readAll
+        if output.len != 0:
+          self.resultBuffer.insertTextAtCaret output
       self.process = nil
-      self.text = self.text + String(self.env.pwd & " $")
-      self.setCaretColumn(10000)
-      self.setCaretLine(10000)
+      self.insertPrompt
 
 proc cd(self: Terminal; cmd: string, args: seq[string]) =
   let path =
@@ -55,10 +87,6 @@ proc cd(self: Terminal; cmd: string, args: seq[string]) =
     self.prevdir = self.env.pwd
     self.env.pwd = expandFilename(self.env.pwd/path)
 
-  self.text = self.text + String(self.env.pwd & " $")
-  self.setCaretColumn(10000)
-  self.setCaretLine(10000)
-
 proc embeddedProcess(self: Terminal; cmd: string, args: seq[string]): bool =
   result = true
   case cmd
@@ -66,8 +94,24 @@ proc embeddedProcess(self: Terminal; cmd: string, args: seq[string]): bool =
     cd(self, cmd, args)
   else:
     result = false
+  if result:
+    self.insertPrompt
 
-proc onWriteBufferTextChanged(self: Terminal) {.gdsync, rename: toGodotInternalFuncCase.} =
+proc isInvalidCaretChange(self: Terminal; c: Caret): bool =
+  case cmp(c.line, self.promptStartAt.line):
+  of int32.low .. -1: true
+  of 0:
+    c.column < self.promptStartAt.column
+  else: false
+
+proc onCaretChanged(self: Terminal) {.gdsync, rename: toGodotInternalFuncCase.} =
+  var caret = self.getCaret
+  if self.isInvalidCaretChange(caret):
+    self.setCaretWithFill self.caret_prev
+  else:
+    self.caret_prev = caret
+
+proc onTextChanged(self: Terminal) {.gdsync, rename: toGodotInternalFuncCase.} =
   if self.text.endsWith("\n"):
     let s = ($self.text)
       .split({'$'})[^1]
@@ -75,7 +119,11 @@ proc onWriteBufferTextChanged(self: Terminal) {.gdsync, rename: toGodotInternalF
       .split(' ')
     let cmd = s[0]
     let args = s[1..^1]
-    if not self.embeddedProcess(cmd, args):
+    if self.embeddedProcess(cmd, args):
+      discard
+    elif findExe(cmd).len != 0:
       self.process = self.env.startProcess(cmd, args)
       self.process.setNonBlock()
-      self.editable = false
+    else:
+      self.resultBuffer.insertTextAtCaret "Unknown command: " & cmd & "\n"
+      self.insertPrompt
